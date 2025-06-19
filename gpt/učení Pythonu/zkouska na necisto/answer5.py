16b62288cd21281864263ef5fe8c8c1f1134d809
Pavel <pavelhrabec368@gmail.com>
2025-06-19 18:12:56 +0200

    zkousky na necisto5

---
+++ gpt/učení Pythonu/zkouska na necisto/answer5.py
@@ -0,0 +1,26 @@
+from exam_lib import User
+
+class VIPUser(User):
+    def __init__(self, name, surname, email, vip_card_number):
+        super().__init__(name, surname, email)
+        if self._check_card(vip_card_number):
+            self._vip_card_number = vip_card_number
+        else:
+            self._vip_card_number = None
+
+    @staticmethod
+    def _check_card(new_number):
+        return isinstance(new_number, int) and new_number > 999 and new_number % 2 == 0
+
+    @staticmethod
+    def use_vip_card():
+        pass  # nebo: return None
+
+    @property
+    def vip_card_number(self):
+        return self._vip_card_number
+
+    @vip_card_number.setter
+    def vip_card_number(self, new_number):
+        if self._check_card(new_number):
+            self._vip_card_number = new_number
---
+++ gpt/učení Pythonu/zkouska na necisto/exam_lib.py
@@ -0,0 +1,5 @@
+class User:
+    def __init__(self, name, surname, email):
+        self.name = name
+        self.surname = surname
+        self.email = email
